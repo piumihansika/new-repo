477d7518ac5e47e405560308dfb3971ca5de8633
piumihansika <piumihansika94.phlg@gmail.com>
2020-11-30 21:53:23 +0530

    First commit

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+       <h1>Hello!!</h1>
+       <h2>
+             Yaayy! You just created your first React Component
+       </h2>
+    </div>
+  );
+}
+
+export default App;
